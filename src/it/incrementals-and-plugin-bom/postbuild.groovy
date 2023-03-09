2644ab73e4d73d7d7ccb6935537a92dd31b626b3
James Nord <jtnord@users.noreply.github.com>
2023-03-09 11:51:48 +0000

    Adds test case for #705
    dependencyManagement can be used to manage the transitive dependency
    versions not only direct ones, and this impacts what can end up in the
    resulting hpi file.
    
    By not including the depMgmt section then any downtream consumer will
    see the un managed versions of the dependencies which will differ from
    what they should see

--- src/it/incrementals-and-plugin-bom/postbuild.groovy
+++ src/it/incrementals-and-plugin-bom/postbuild.groovy
@@ -1,3 +1,19 @@
 assert new File(basedir, '../../local-repo/io/jenkins/plugins/incrementals-and-plugin-bom/1.0-SNAPSHOT/incrementals-and-plugin-bom-1.0-SNAPSHOT.hpi').file
 assert new File(basedir, '../../local-repo/io/jenkins/plugins/incrementals-and-plugin-bom/1.0-rc1234.deadbeef5678/incrementals-and-plugin-bom-1.0-rc1234.deadbeef5678.hpi').file
+String pomXml = new File(basedir, '../../local-repo/io/jenkins/plugins/incrementals-and-plugin-bom/1.0-rc1234.deadbeef5678/incrementals-and-plugin-bom-1.0-rc1234.deadbeef5678.pom').text
+assert pomXml.contains('version>1.0-rc1234.deadbeef5678</version>')
+// https://github.com/jenkinsci/plugin-pom/issues/705
+// line endings need normalising
+assert pomXml.replace("\r\n", "\n").contains('''
+  <dependencyManagement>
+    <dependencies>
+      <dependency>
+        <groupId>io.jenkins.tools.bom</groupId>
+        <artifactId>bom-2.361.x</artifactId>
+        <version>1580.v47b_429a_c853a</version>
+        <type>pom</type>
+        <scope>import</scope>
+      </dependency>
+    </dependencies>
+  </dependencyManagement>'''.replace("\r\n", "\n"))
 return true
