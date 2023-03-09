d8e7f3a474b45c975f50371b33f0643b01269d17
James Nord <jtnord@users.noreply.github.com>
2023-03-09 17:12:42 +0000

    switch to expand and adapt test case accordingly
    as discussed with @jglick as the intention of the flatten plugin here is to have
    a self contained pom - this will switch to expand which will also have the benefit
    of picking up any <dependencyManagement> configured in the parent(s).

--- src/it/incrementals-and-plugin-bom/postbuild.groovy
+++ src/it/incrementals-and-plugin-bom/postbuild.groovy
@@ -4,16 +4,10 @@ String pomXml = new File(basedir, '../../local-repo/io/jenkins/plugins/increment
 assert pomXml.contains('version>1.0-rc1234.deadbeef5678</version>')
 // https://github.com/jenkinsci/plugin-pom/issues/705
 // line endings need normalising
-assert pomXml.replace("\r\n", "\n").contains('''
-  <dependencyManagement>
-    <dependencies>
-      <dependency>
-        <groupId>io.jenkins.tools.bom</groupId>
-        <artifactId>bom-2.361.x</artifactId>
-        <version>1580.v47b_429a_c853a</version>
-        <type>pom</type>
-        <scope>import</scope>
-      </dependency>
-    </dependencies>
-  </dependencyManagement>'''.replace("\r\n", "\n"))
+assert pomXml.matches('(?s).*<dependencyManagement>\\s*<dependencies>'
+// something from the parent pom dependencyManagement
++ '.*<dependency>\\s*<groupId>javax.servlet</groupId>\\s*<artifactId>javax.servlet-api</artifactId>\\s*<version>3.1.0</version>\\s*</dependency>'
+// something from the bom
++ '.*<dependency>\\s*<groupId>io.jenkins.plugins</groupId>\\s*<artifactId>caffeine-api</artifactId>\\s*<version>2.9.3-65.v6a_47d0f4d1fe</version>\\s*</dependency>'
++ '.*</dependencies>\\s*</dependencyManagement>.*')
 return true
