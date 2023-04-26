3bed3c929c109e09f27bfbe4b45d609453eb6393
Jérôme Pochat <jpochat@cloudbees.com>
2023-04-26 12:33:49 +0200

    Fix Spotless issues

--- src/it/servlet-api/src/test/java/test/ServletAPITest.java
+++ src/it/servlet-api/src/test/java/test/ServletAPITest.java
@@ -1,11 +1,10 @@
 package test;
 
+import hudson.security.LegacySecurityRealm;
+import jenkins.model.Jenkins;
 import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
-
-import hudson.security.LegacySecurityRealm;
-import jenkins.model.Jenkins;
 
 public class ServletAPITest {
 
@@ -15,14 +14,14 @@ public class ServletAPITest {
     /**
      * When having both Servlet APIs 3.1 and 4.0 in classpath, the following error
      * is logged on server side:
-     * 
+     *
      * <pre>
      * WARNING	o.e.jetty.server.HttpChannel#handleException: /jenkins/j_security_check
      * java.lang.AbstractMethodError: Receiver class org.eclipse.jetty.security.authentication.SessionAuthentication does not define or inherit an
      * implementation of the resolved method 'abstract void valueBound(javax.servlet.http.HttpSessionBindingEvent)' of interface
      * javax.servlet.http.HttpSessionBindingListener. at org.eclipse.jetty.server.session.Session.bindValue(Session.java:357)
      * </pre>
-     * 
+     *
      * And then on client side getting "500 Server Error for
      * http://localhost:.../jenkins/j_security_check"
      */
