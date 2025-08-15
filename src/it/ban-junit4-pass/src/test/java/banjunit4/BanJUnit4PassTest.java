10a9d690910eed6e9a0bed1f9b4bac09bf5dfe4c
strangelookingnerd <49242855+strangelookingnerd@users.noreply.github.com>
2025-08-15 13:47:53 +0200

    Allow `org.junit.platform.**` imports

--- src/it/ban-junit4-pass/src/test/java/banjunit4/BanJUnit4PassTest.java
+++ src/it/ban-junit4-pass/src/test/java/banjunit4/BanJUnit4PassTest.java
@@ -1,9 +1,11 @@
 package banjunit4;
 
 import org.junit.jupiter.api.Test;
+import org.junit.platform.suite.api.Suite;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@Suite
 public class BanJUnit4PassTest {
     @Test
     public void thisFails() {
