3d7cb1c8e3431e568cf2ad56fa9bcdf900f82421
Daniel Beck <daniel-beck@users.noreply.github.com>
2025-08-15 10:09:32 +0200

    Add tests

---
+++ src/it/ban-juni4-fail/postbuild.groovy
@@ -0,0 +1,12 @@
+// Build failed
+def hpi = new File(basedir, 'target/ban-junit4-fail-it.jar')
+assert !hpi.exists()
+
+// Because of banned imports
+def log = new File(basedir, 'build.log')
+assert log.text.contains('Reason: Use JUnit 5 (org.junit.jupiter.*)')
+assert log.text.contains('org.junit.Test')
+assert log.text.contains('(Line: 3, Matched by: org.junit.**)')
+assert log.text.contains('static org.junit.Assert.assertTrue')
+assert log.text.contains('(Line: 5, Matched by: org.junit.**)')
+return true
---
+++ src/it/ban-juni4-fail/src/test/java/banjunit4/BanJUnit4FailTest.java
@@ -0,0 +1,12 @@
+package banjunit4;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class BanJUnit4FailTest {
+    @Test
+    public void thisFails() {
+        assertTrue(true);
+    }
+}
