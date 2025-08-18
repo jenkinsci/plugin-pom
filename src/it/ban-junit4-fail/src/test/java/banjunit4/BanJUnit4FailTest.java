76a47b2ed4ddc0aeccc7a0b778ea9f7ebad4ed0f
strangelookingnerd <49242855+strangelookingnerd@users.noreply.github.com>
2025-08-18 10:20:34 +0200

    Ban Junit3 imports

--- src/it/ban-junit4-fail/src/test/java/banjunit4/BanJUnit4FailTest.java
+++ src/it/ban-junit4-fail/src/test/java/banjunit4/BanJUnit4FailTest.java
@@ -3,10 +3,12 @@ package banjunit4;
 import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
+import static junit.framework.Assert.assertFalse;
 
 public class BanJUnit4FailTest {
     @Test
     public void thisFails() {
         assertTrue(true);
+        assertFalse(false);
     }
 }
