06e085cf51c31428453de90e0db8da4c0469509c
strangelookingnerd <49242855+strangelookingnerd@users.noreply.github.com>
2025-12-16 22:37:33 +0100

    Fix ban-junit4-pass

--- src/it/ban-junit4-pass/src/test/java/banjunit4/BanJUnit4PassTest.java
+++ src/it/ban-junit4-pass/src/test/java/banjunit4/BanJUnit4PassTest.java
@@ -5,7 +5,7 @@ import org.junit.platform.suite.api.Suite;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@Suite
+@Suite(failIfNoTests = false)
 public class BanJUnit4PassTest {
     @Test
     public void thisFails() {
