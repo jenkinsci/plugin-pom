3cfc7e2468bddae41198165f0aab050dc1ae4825
Jesse Glick <jglick@cloudbees.com>
2026-04-27 16:14:13 -0400

    bump

--- src/it/bom-obsolete-override-skip-property/postbuild.groovy
+++ src/it/bom-obsolete-override-skip-property/postbuild.groovy
@@ -1,6 +1,6 @@
 def log = new File(basedir, 'build.log').getText('UTF-8')
 // Verify the rule was skipped via property
-assert log.contains('Skipping RequireNonObsoleteDependencyManagement rule')
+assert log.contains('[RequireNonObsoleteDependencyManagement] Skipping rule')
 // Should NOT contain the error message
 assert !log.contains('Found obsolete dependency version overrides')
 return true
